d7cd8bb2ef869aac2cfdf54251a1f2cab01ffaf0
gozhien <naan7714@gmail.com>
2026-02-18 11:32:49 +0700

    Fix ke root

---
+++ service/src/main/java/com/github/kr328/clash/service/store/ZivpnStore.kt
@@ -0,0 +1,35 @@
+package com.github.kr328.clash.service.store
+
+import android.content.Context
+import com.github.kr328.clash.common.store.Store
+import com.github.kr328.clash.common.store.asStoreProvider
+import com.github.kr328.clash.service.PreferenceProvider
+
+class ZivpnStore(context: Context) {
+    private val store = Store(
+        PreferenceProvider
+            .createSharedPreferencesFromContext(context)
+            .asStoreProvider()
+    )
+
+    var serverHost: String by store.string(
+        key = "zivpn_server_host",
+        defaultValue = ""
+    )
+
+    var serverPass: String by store.string(
+        key = "zivpn_server_pass",
+        defaultValue = ""
+    )
+
+    var serverObfs: String by store.string(
+        key = "zivpn_server_obfs",
+        defaultValue = "hu``hqb`c"
+    )
+    
+    // Comma separated ranges
+    var portRanges: String by store.string(
+        key = "zivpn_port_ranges",
+        defaultValue = "6000-7750,7751-9500,9501-11250,11251-13000,13001-14750,14751-16500,16501-18250,18251-19999"
+    )
+}
